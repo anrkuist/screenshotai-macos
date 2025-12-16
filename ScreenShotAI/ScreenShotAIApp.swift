3e510df55b4e5f9a1032e17acf63f1e79682390c
anrkuist <anrkuist@protonmail.com>
2025-12-16 17:24:46 -0300

    Initial Commit

---
+++ ScreenShotAI/ScreenShotAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAIApp.swift
+//  ScreenShotAI
+//
+//  Created by augusto on 16/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenShotAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenShotAITests/ScreenShotAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenShotAITests.swift
+//  ScreenShotAITests
+//
+//  Created by augusto on 16/12/2025.
+//
+
+import Testing
+@testable import ScreenShotAI
+
+struct ScreenShotAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
